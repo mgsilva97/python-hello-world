98d70010e93a616a42d1a731f4af092c337761d4
Michael <mgsilva97@outlook.com>
2020-11-12 13:53:48 -0300

    Ajuste na configuracao do x-ray

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,9 @@
 from aws_xray_sdk.core import xray_recorder
 
     xray_recorder.begin_segment('x-ray-recorder')
+        xray_recorder.begin_subsegment('x-ray-recorder-sub')
 
         print ('hello world!')
-    
+
+        xray_recorder.end_subsegment()
     xray_recorder.end_segment()
