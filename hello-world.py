f3eaa0b6e162520745fbe36b8ebff38a4ca92ea0
Michael <mgsilva97@outlook.com>
2020-11-12 15:49:16 -0300

    ajuste de indentacao

--- hello-world.py
+++ hello-world.py
@@ -1,12 +1,7 @@
-
 #!/usr/bin/env python
-
 from aws_xray_sdk.core import xray_recorder
-
-    xray_recorder.begin_segment('x-ray-recorder')
-        xray_recorder.begin_subsegment('x-ray-recorder-sub')
-
-        print ('hello world!')
-
-        xray_recorder.end_subsegment()
-    xray_recorder.end_segment()
+xray_recorder.begin_segment('x-ray-recorder')
+xray_recorder.begin_subsegment('x-ray-recorder-sub')
+print ('hello world!')
+xray_recorder.end_subsegment()
+xray_recorder.end_segment()
